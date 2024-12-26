8d0a205dc8b0c61980cd1a92c387fdcaa176a0a8
Amka <a.amkush@gmail.com>
2024-12-27 01:18:01 +0800

    geriin daalgawar 2

--- Homework2/#Data_clear.py
+++ Homework2/#Data_clear.py
@@ -1,5 +1,6 @@
 #Data_clear
 import pandas as pd
+import re
 
 main_path = 'Homework2/'
 df0 = pd.read_csv(main_path + 'python_course_py4econ/5_Visualization/data/2404.csv')
@@ -11,7 +12,8 @@ df4 = pd.read_csv(main_path + 'python_course_py4econ/5_Visualization/data/2410.c
 #Bosoogoor negtgeh
 df = pd.concat([df0, df1, df2, df3, df4])
 
-df
+df.columns
+
 
 #Baganii ner uurchlult
 df = df.rename(columns={
@@ -27,12 +29,41 @@ df = df.rename(columns={
 
 #Count NA
 
-na_count = df.isna().sum().sum()
+df.isna().sum()
+df.isna().sum().sum()
 
 #Duplicated
+df.duplicated().sum()
 
-date_duplicated = df['Date'].duplicated().sum()
+df[df.duplicated()]
 
+df.duplicated(subset=['Date', 'time', 'ad_id'])
+duplicated_rows = df[df.duplicated(subset=['Date', 'time', 'ad_id'], keep=False)]
+duplicated_rows['ad_id'].value_counts()
+duplicated_rows[duplicated_rows['ad_id'] == 8526587]
 
+none_duplicate = duplicated_rows.drop_duplicates(subset=['Date', 'time', 'ad_id'], keep='first')
+
+none_duplicate[none_duplicate['ad_id'] == 8526587]
+
+#Une bolon talbain utgiig toon utga ruu shiljvvleh
+
+df[['Price_Total', 'size']].dtypes
+
+df['new_size'] = df['size'].apply(lambda x: re.findall(r'\d+[\.\d]*', x)[0]).astype(float)
+df[['new_size', 'size']]
+
+df[['Price_Total']]
+
+def extract_numeric(value):
+    numeric_part = re.findall(r'\d+[\.\d]*', value)
+    return float(numeric_part[0]) if numeric_part else None
+df['new_Price_Total'] = df['Price_Total'].apply(extract_numeric)
+df['new_Price_Total']
+
+df[['new_Price_Total', 'new_size']].dtypes
+df[['new_Price_Total', 'Price_Total']]
+
+df[['price_m2']]
 
 
